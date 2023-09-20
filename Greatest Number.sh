af6cccc3be1fb0235407c54162a72a642b159f25
divyesh-mali <125090897+divyesh-mali@users.noreply.github.com>
2023-09-20 11:23:10 +0530

    Add files via upload

---
+++ Greatest Number.sh
@@ -0,0 +1,19 @@
+# Shell script to find greatest of three numbers
+
+echo "Enter Num 1"
+read num1
+echo "Enter Num 2"
+read num2
+echo "Enter Num 3"
+read num3
+
+if [ $num1 -gt $num2 ] && [ $num1 -gt $num3 ]
+then 
+    echo $num1 "is greatest number"
+elif [ $num2 -gt $num1 ] && [ $num2 -gt $num3 ]
+then    
+    echo $num2 "is greatest number"
+else
+    echo $num3 "is greatest number"
+fi
+    
